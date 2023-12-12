277313f4960f5f8e0fbd1c4cb7d4a1f5d317fcec
chaikritt <techaploog@gmail.com>
2023-12-12 12:15:03 +0700

    add authentication

---
+++ middleware.ts
@@ -0,0 +1,25 @@
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+ 
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
+
+const publicRoutes = [
+  '/',
+  '/sign-in',
+  '/sign-up',
+];
+
+export default authMiddleware({
+  publicRoutes,
+  afterAuth(auth,req,evt) {
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+  }
+});
+ 
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
+ 
