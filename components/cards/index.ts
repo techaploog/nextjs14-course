cd2a88d2b299a08b5a9ab6008c85a94f672f414d
chaikritt <techaploog@gmail.com>
2023-12-17 22:36:19 +0700

    add tags page

--- components/cards/index.ts
+++ components/cards/index.ts
@@ -1,2 +1,3 @@
 export * from "./question-card.component";
 export * from "./user-card.component";
+export * from "./tag-card.component";
--- lib/actions/tag.actions.ts
+++ lib/actions/tag.actions.ts
@@ -1,8 +1,8 @@
 "use server";
 
-import { User } from "@/database";
+import { Tag, User } from "@/database";
 import { connectToDatabase } from "../utils";
-import { GetTopInteractedTagsParams } from "./share.types";
+import { GetAllTagsParams, GetTopInteractedTagsParams } from "./share.types";
 
 export async function getTopInteratedTags(params: GetTopInteractedTagsParams) {
   try {
@@ -18,6 +18,18 @@ export async function getTopInteratedTags(params: GetTopInteractedTagsParams) {
       { _id: "2", name: "tag2" },
       { _id: "3", name: "tag3" },
     ];
+  } catch (error: any) {
+    console.log("[ERROR]", error.message);
+    throw error;
+  }
+}
+
+export async function getAllTags(params: GetAllTagsParams) {
+  try {
+    connectToDatabase();
+    const tags = await Tag.find({});
+
+    return { tags };
   } catch (error: any) {
     console.log("[ERROR]", error.message);
     throw error;
